6a88831b1149ef241eca53b81fde7540cc7b2ac3
yuanyougao <yuanyougao@163.com>
2017-12-03 22:08:41 +0800

    提交

---
+++ src/main/java/chapter2/CopyOfReflectServiceImpl1.java
@@ -0,0 +1,55 @@
+package chapter2;
+
+import java.lang.reflect.Method;
+
+
+public class CopyOfReflectServiceImpl1 {
+	
+	 public CopyOfReflectServiceImpl1 () {
+		 
+	 }
+	 
+	 public CopyOfReflectServiceImpl1 getInit () {
+		 CopyOfReflectServiceImpl1 copyof = null ; 
+		 try {
+			 copyof =(CopyOfReflectServiceImpl1)Class.forName("ssm.chapter2.reflect.CopyOfReflectServiceImpl1")
+					 .newInstance() ; 
+		 } catch ( Exception ex ) {
+			 ex.printStackTrace(); 
+		 }
+		 
+		 return  copyof ;
+	 }
+	 public void sayHi (String name ,Integer age ) {
+		 System.out.println(" sayHi " + name +" ��ͣ�" + age  );
+	 }
+	 public static void main(String[] args) {
+		 
+		 System.out.println("hello");
+		 CopyOfReflectServiceImpl1 copyof = null ; 
+		 
+		 try {
+			 copyof =(CopyOfReflectServiceImpl1)Class.forName("ssm.chapter2.CopyOfReflectServiceImpl1")
+					 .newInstance() ; 
+		 } catch ( Exception ex ) {
+			 ex.printStackTrace(); 
+		 }
+		 try {
+			Method  method =copyof.getClass().getMethod("sayHi", String.class,Integer.class) ;
+			//method.invoke(copyof, "liuliu" , new Integer("25") );
+			
+			method.invoke(copyof, "liuliu" ,   Integer.valueOf("25")  );
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 
+	}
+}
---
+++ src/main/java/chapter2/ReflectServiceImpl.java
@@ -0,0 +1,61 @@
+package chapter2;
+
+import java.lang.reflect.Method;
+
+
+public class ReflectServiceImpl {
+	
+	private String name ; 
+	public ReflectServiceImpl (String name) {
+		this.name = name ;
+	}
+	public static ReflectServiceImpl getInstance () {
+		ReflectServiceImpl object  = null ;
+		try {
+			object = (ReflectServiceImpl) Class.forName("ssm.chapter2.reflect.ReflectServiceImpl")
+					.newInstance() ; 
+		} catch (Exception ex ) {
+			ex.printStackTrace();
+		}
+		
+		return object ;
+	}
+	
+	public static ReflectServiceImpl getInstance1 () {
+		ReflectServiceImpl object  = null ;
+		try {
+			object = (ReflectServiceImpl) Class.forName("ssm.chapter2.reflect.ReflectServiceImpl")
+					.getConstructor(String.class).newInstance("liuju") ; 
+		} catch (Exception ex ) {
+			ex.printStackTrace();
+		}
+		
+		return object ;
+	}
+	public void sayHello (String name) {
+		System.err.println("Hello" + name );
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReflectServiceImpl object = ReflectServiceImpl.getInstance1();
+		
+		try {
+			Method method =  ReflectServiceImpl.class.getMethod("sayHello",String.class) ;
+			method.invoke(object, "liuliu");
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//   cSystem.out.println(object.name) ; 
+		/*ReflectServiceImpl  impl =new ReflectServiceImpl () ;
+		impl.sayHello("impl");
+		object.sayHello("hello");*/
+		
+		
+	}
+
+}
