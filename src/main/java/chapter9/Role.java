1e420c76fbaa71aba92177650947a994a042de8b
yuanyougao <yuanyougao@163.com>
2018-01-30 08:13:11 +0800

    commit

---
+++ src/main/java/chapter9/Role.java
@@ -0,0 +1,41 @@
+package chapter9;
+public class Role {
+
+	private int id;
+	private String rolename;
+	private String note;
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getRolename() {
+		return rolename;
+	}
+
+
+	public void setRolename(String rolename) {
+		this.rolename = rolename;
+	}
+
+
+	public String getNote() {
+		return note;
+	}
+
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
