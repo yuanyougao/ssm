d951d161b7b2b9a3bb8b15c78701992a15567548
Administrator <Administrator@YOS-01906011418>
2019-06-02 12:12:17 +0800

    commit

---
+++ src/main/java/chapter14/pojo/Users.java
@@ -0,0 +1,20 @@
+package chapter14.pojo;
+
+public class Users {
+
+	String username;
+	String userpwd;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUserpwd() {
+		return userpwd;
+	}
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
+}
